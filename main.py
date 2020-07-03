7a0ea078072738aa3f088deae0f8fb9469d66956
nhattieu <tepzxd@gmail.com>
2020-07-03 22:25:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# print('hello world')
+
+# day1 = ('monday', 'tuesday', 'wednesday')
+# day2 = ('thursday', 'friday', 'saturday' , 'sunday')
+# day3 = ('thursday', 'friday', 'saturday' , 'sunday', day1)
+
+# day = day1 + day2
+# print('Day: ', end="")
+# print(day)
+# print(day3)
+# print(day3[1][0][0])
+# a = 7 // 4.0
+# print(type(a))
+
+# from decimal import Decimal
+
+# print(Decimal(0.1 + 0.2))
+# print(Decimal(0.3))
+
+myString = 'abcdefgh'
+
+print(len(myString))
+
+print(myString[1:])
+print(myString[1:-1])
+print(myString[:])
+
